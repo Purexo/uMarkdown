7a11df45bbe6c86585abaaf2966d6e73edbfa1fd
Purexo <theotime93@yahoo.fr>
2015-02-06 14:10:33 +0100

    mode automatique fait et testé. seul un parametre est obligatoire (fichier "source") le fichier de sortis est optionnel. si il n'est pas précisé il seras sous le meme nom + ".html" dans le meme dossier
    reorganisation du code __un peu__ plus propres en prévision d'un systeme
    de parametre avancé, et de la gui.

--- src/com/github/purexo/umarkdown/main/Main.java
+++ src/com/github/purexo/umarkdown/main/Main.java
@@ -1,6 +1,7 @@
 package com.github.purexo.umarkdown.main;
 import java.io.File;
 import java.io.FileWriter;
+import java.io.IOException;
 
 import org.markdown4j.Markdown4jProcessor;
 
@@ -16,34 +17,43 @@ public class Main {
 	 * 	2: Adresse du fichier markdown incorrecte \n
 	 * 	3: Adresse du fichier html incorrecte
 	 */
+	
 	public static void main(String[] args) throws Exception {
+		String[] targs = {
+			"G:\\Travail\\Perso\\Java\\µmarkdown\\test\\test.md"
+			// ,"C:\\Users\\Théotime\\Desktop\\test.html"
+		};
+		File fEntree;
+		File fSortie;
+		FileWriter fwSortie;
 		String html;
 		
-		String[] targs = {"C:\\Users\\Théotime\\Desktop\\test.md", "C:\\Users\\Théotime\\Desktop\\test.html"};
-		
-		if (args.length < 2) throw new Exception("1: nb de parametres inssufisant");
-		
-		File fEntree = new File(args[0]);
-		File fSortie = new File(args[1]);
-		FileWriter fwSortie = new FileWriter(fSortie);
-		
-		if (fEntree.exists() && fEntree.canRead() && fEntree.isFile())
-			html = new Markdown4jProcessor().process(fEntree);
-		else {
-			fwSortie.close();
-			throw new Exception("2: Adresse du fichier markdown incorrecte");
+		switch (args.length) {
+			case 0:
+				throw new Exception("1: nb de parametres inssufisant (GUI non implanté)");
+			case 1:
+				fEntree = new File(args[0]);
+				fSortie = new File(autoName(fEntree));
+				break;
+			default:
+				fEntree = new File(args[0]);
+				fSortie = new File(args[1]);
+				break;
+				
 		}
-		
-		if (fSortie.canWrite()) {
-			fSortie.createNewFile(); 									// Creer un nouveau fichier au besoin
-			fwSortie.flush();											// Le nettoie avant écriture
-			fwSortie.write(html);
-			fwSortie.close();
-		} else {
+
+		html = mdToHTML(fEntree); // peut couper le programme (throw exception n°2)
+		fwSortie = new FileWriter(fSortie);
+
+		if (!fSortie.canWrite()) {
 			fwSortie.close();
 			throw new Exception("3: Adresse du fichier html incorrecte");
 		}
-		
+		fSortie.createNewFile(); 									// Creer un nouveau fichier au besoin
+		fwSortie.flush();											// Le nettoie avant écriture
+		fwSortie.write(html);
+		fwSortie.close();
+
 		String success = "" +
 		"Votre fichier " + fEntree.getName() + "\n" +
 		"à été Parsé vers \n" +
@@ -51,6 +61,39 @@ public class Main {
 		"Avec Succes.";
 		
 		System.out.println(success);
+	}
+	
+	/**
+	 * 
+	 * @param fEntree
+	 * @return
+	 * @throws Exception
+	 */
+	private static String mdToHTML(File fEntree) throws Exception {
+		if (fEntree.exists() && fEntree.canRead() && fEntree.isFile())
+			return new Markdown4jProcessor().process(fEntree);
+		else {
+			throw new Exception("2: Adresse du fichier markdown incorrecte");
+		}
+	}
+	
+	/**
+	 * prend un fichier
+	 * @param f un Fichier
+	 * @return le nom du fichier - l'extension + ".html"
+	 */
+	private static String autoName(File f) {
+		return f.getParent() + "/" + f.getName() + ".html";
+		/*
+		String[] tab = f.getName().split(".");
+		String fName = "";
+		
+		for (int i = 0; i < tab.length - 1; i++)
+			fName += tab[i];
+		
+		String str = fName + ".html";
+		return str;
+		*/
 	}
 	
 	/*
---
+++ test/test.md
@@ -0,0 +1,25 @@
+Comme vous le savez probablement (sinon sortez de votre grotte !) Il y a eu un attentat hier (mercredi 7 janvier 2015) contre Charli Hebdo, le très célèbre journal satirique, ~~et régulièrement polémiste~~ (pour ceux qui n'ont pas d'humour, et incapable de supporter une critique)
+
+Le peuple est en émois, le monde est en deuil, les médias à l'affuts du moindre événement à couvrir profitent pour faire de l'audimat. Bien sur je suis désolé par cet événement, voir des **terroristes** (Ils sont peut être islamiste mais ce sont des extrémistes et je tiens à rappeler que terroriste ne veut pas dire islamiste et vice versa...) s'attaquer à un symbole de la liberté d'expression c'est inadmissible !
+
+J'ai été amenée à répondre à quelqu'un dans les commentaire d'une vidéo Youtube : <https://purexo.eu/shaarli/?06eksw>  
+Tout ça pour rappeler que ce n'est pas la religion le problème, mais les personnes capables d'accomplir des atrocité sous couverts de celle ci.
+
+Mais au final, ce qui me désole le plus, c'est de voir que médias et politiciens profitant de la situation pour toujours plus enfoncé les citoyens dans un climat d'insécurité.
+
+* [Marine Le Pen qui propose de réinstaurer la peine de mort ?](https://oniricorpe.eu/links/?HDUUxQ) On est plus au moyen age...
+
+Mais au final, [les dernières lois](https://bourgoinblog.wordpress.com/2014/09/21/la-nouvelle-loi-antiterroriste-est-liberticide-et-inefficace/) ( [liberticides je le rappel](https://bourgoinblog.wordpress.com/2014/12/27/la-loi-de-programmation-militaire-va-legaliser-la-surveillance-dinternet/comment-page-1/) ) votés, les plans vigi-pirates augmenté ont ils aidés sur cet événement ? Je ne pense pas, le mal à été fait.
+
+Depuis que je suis enfant, on me rabâche que l'on vis dans une démocratie, ce qui inclue la liberté d'expression, sauf qu'avec le temps je vois les ficèles qui ce désagrègent et j'aperçois ce qui ce passent derrière le rideau pendant que tout le monde reste assis tranquillement sur son fauteuil, admirant le spectacle, comme si de rien était.
+
+Mes pensées vont bien évidement au familles des personnes assassinés et des victimes de cet attentat (journalistes, policiers, gendarmes, ou tout autre personnes).
+
+### Des soutients qui m'ont touchés :
+
+* [La complainte affranchie](https://oniricorpe.eu/blog/article6/la-complainte-affranchie)
+* [Compilation de dessins par Geoffrey Dorne](http://graphism.fr/les-dessinateurs-mobiliss-pour-charliehebdo/)
+* [C'est l'encre qui doit couler, pas le sang](https://hackingsocialblog.wordpress.com/2015/01/08/cest-lencre-qui-doit-couler-pas-le-sang/)
+* [#JeSuisCharlie - JB Bullet](https://www.youtube.com/watch?v=-bjbUg9d64g)
+ 
+Je met en relation cet article avec [La France a peur: le syndrome du grand méchant monde](https://www.youtube.com/watch?v=8WiiqssAME4), une vidéo Youtube intéressante concernant la manipulation des masses par les médias.
